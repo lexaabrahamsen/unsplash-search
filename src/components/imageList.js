6e4df9607f3092c05273938de7a1b733e1cbc38f
Lexa <lexaraea@gmail.com>
2020-01-20 15:30:24 -0500

    Add masonry grid style to photos list

--- src/components/imageList.js
+++ src/components/imageList.js
@@ -1,11 +1,10 @@
 import React from "react";
 import "./imageList.css";
+import ImageCard from "./ImageCard";
 
 const ImageList = props => {
   const imgs = props.foundImages.map(img => {
-    return (
-      <img key={img.id} src={img.urls.regular} alt={img.alt_description} />
-    );
+    return <ImageCard key={img.id} image={img} />;
   });
 
   return <div className="image__list">{imgs}</div>;
