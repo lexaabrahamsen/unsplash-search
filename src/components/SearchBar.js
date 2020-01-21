686b6c8069502140d5f10c3f8f9dde42348fb986
Lexa <lexaraea@gmail.com>
2020-01-20 23:12:02 -0500

    Update design

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -18,7 +18,8 @@ class SearchBar extends React.Component {
       <div>
         <form onSubmit={this.onFormSubmit} className="flexContainer">
           <label>
-            <h1 className="h1">Image Search</h1>
+            <h3 className="">WHAT ARE YOU LOOKING FOR</h3>
+            <h1 className="h1">WHAT ARE YOU LOOKING FOR</h1>
           </label>
           <input
             className="inputStyle"
@@ -26,9 +27,7 @@ class SearchBar extends React.Component {
             value={this.state.val}
             onChange={this.onInputChange}
           />
-          <button value={this.state.val} onChange={this.onInputChange}>
-            Search
-          </button>
+          <button onChange={this.onInputChange}>Search</button>
         </form>
       </div>
     );
